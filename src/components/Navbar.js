8628ea61c2d2e5fc636d06eeebc9348d42e2783d
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2023-12-05 12:12:33 +0530

    Comments added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,24 +7,18 @@ import Link from "next/link";
  * @return {React.Element} The rendered Navbar component.
  */
 const Navbar = () => {
-    return (<header
-        className='w-full px-32 py-8 font-medium'
-    >
-        <nav>
-            <Link href="/">
-                Home
-            </Link>
-            <Link href="/about">
-                About
-            </Link>
-            <Link href="/projects">
-                Projects
-            </Link>
-            <Link href="/articles">
-                Articles
-            </Link>
-        </nav>
-    </header>)
-}
+    return (
+        // Render the header element with the specified classes
+        <header className='w-full px-32 py-8 font-medium'>
+            <nav>
+                {/* Render the links */}
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/articles">Articles</Link>
+            </nav>
+        </header>
+    );
+};
 
 export default Navbar
