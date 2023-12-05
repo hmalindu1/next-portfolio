a7c03d0f1bbad096a8a6bd8c006cf51eedbca925
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2023-12-05 12:11:28 +0530

    Navbar Component added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,34 @@
+import Navbar from '@/components/Navbar';
 import '@/styles/globals.css'
-import {Montserrat} from "next/font/google"
+import { Montserrat } from "next/font/google"
 import Head from "next/head";
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-mont"
+    subsets: ["latin"],
+    variable: "--font-mont"
 })
+/**
+ * Renders the main application component.
+ * 
+ * @param {Object} props - The component props.
+ * @param {React.ComponentType} props.Component - The component to render.
+ * @param {Object} props.pageProps - The props to pass to the component.
+ * @returns {JSX.Element} The rendered component.
+ */
 export default function App({ Component, pageProps }) {
-  return (
-      <>
-          <Head>
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
-              <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
-              <Component {...pageProps} />
-          </main></>
-      )
+    return (
+        <>
+            {/* Set the viewport and favicon */}
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+                {/* Render the Navbar component */}
+                <Navbar/> 
+                {/* Render the specified component with the given props */}
+                <Component {...pageProps} />
+            </main>
+        </>
+    )
 }
