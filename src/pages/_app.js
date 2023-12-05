0542d9b7d0f4637134351ae9f586fa205f8ce72a
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2023-12-05 11:37:41 +0530

    Added css properties to the main tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import {Montserrat} from "next/font/google"
+import Head from "next/head";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -7,8 +8,13 @@ const montserrat = Montserrat({
 })
 export default function App({ Component, pageProps }) {
   return (
-      <main className={`${montserrat.variable} font-mont`}>
-          <Component {...pageProps} />
-      </main>
+      <>
+          <Head>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+              <Component {...pageProps} />
+          </main></>
       )
 }
