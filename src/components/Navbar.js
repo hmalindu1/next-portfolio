c5d1e81228c593c1407f3f816783aa6cd321868b
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2023-12-08 13:28:38 +0530

    Social media components added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,13 +9,22 @@ import Link from "next/link";
 const Navbar = () => {
     return (
         // Render the header element with the specified classes
-        <header className='w-full px-32 py-8 font-medium'>
+        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
             <nav>
                 {/* Render the links */}
                 <Link href="/">Home</Link>
                 <Link href="/about">About</Link>
                 <Link href="/projects">Projects</Link>
                 <Link href="/articles">Articles</Link>
+            </nav>
+            <h1>Logo</h1>
+            <nav>
+                <Link href="/" target='_blank'>T</Link>
+                <Link href="/" target='_blank'>T</Link>
+                <Link href="/" target='_blank'>T</Link>
+                <Link href="/" target='_blank'>T</Link>
+                <Link href="/" target='_blank'>T</Link>
+                <Link href="/" target='_blank'>T</Link>
             </nav>
         </header>
     );
