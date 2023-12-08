a5b853e3e62fb8aaa346b57216a0b4caa4c6be0a
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2023-12-08 16:29:48 +0530

    Logo color changed to two

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -18,15 +18,8 @@ const Logo = () => {
         href="/"
         className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold"
         whileHover={{
-          backgroundColor: [
-            "#121212",
-            "rgba(131,58,180,1)",
-            "rgba(253,29,29,1)",
-            "rgba(252,176,69,1)",
-            "rgba(131,58,180,1)",
-            "#121212",
-          ],
-          transition: { duration: 1, repeat: Infinity },
+          backgroundColor: ["#487de7", "#70369d", "#487de7"],
+          transition: { duration: 5, repeat: Infinity },
         }}
       >
         HM
