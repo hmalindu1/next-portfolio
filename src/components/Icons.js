60a344c53ce6a689e3c135083f330210cbb01753
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2023-12-12 23:59:07 +0530

    Audit Log for Icon component file

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,3 +1,10 @@
+/* ==================================== Audit Log ======================================== *
+ * Title: Icon component
+ * Author: Hashan
+ * Created on: 8 Dec 2023
+ * Description: Icons for Navbar
+/* ======================================================================================== */
+
 import * as React from "react";
 
 export const GithubIcon = ({ className, ...rest }) => (
@@ -44,7 +51,7 @@ export const StackOverflowIcon = ({ className, ...rest }) => (
     width={2120}
     height={2500}
     viewBox="0 0 169.61 200"
-    className={`w-8 h-auto ${className}`}
+    className={`w-4 h-auto ${className}`}
     {...rest}
   >
     <path
@@ -65,7 +72,7 @@ export const MediumIcon = ({ className, ...rest }) => (
     height={800}
     preserveAspectRatio="xMidYMid"
     viewBox="0 -55 256 256"
-    className={`w-8 h-auto ${className}`}
+    className={`w-5 h-auto ${className}`}
     {...rest}
   >
     <path d="M72.2 0c39.877 0 72.2 32.549 72.2 72.696 0 40.148-32.326 72.694-72.2 72.694-39.872 0-72.2-32.546-72.2-72.694C0 32.55 32.325 0 72.2 0Zm115.3 4.258c19.938 0 36.101 30.638 36.101 68.438h.003c0 37.791-16.163 68.438-36.1 68.438-19.939 0-36.101-30.647-36.101-68.438 0-37.79 16.16-68.438 36.098-68.438Zm55.803 7.129c7.011 0 12.697 27.449 12.697 61.31 0 33.85-5.684 61.31-12.697 61.31-7.013 0-12.694-27.452-12.694-61.31 0-33.859 5.684-61.31 12.694-61.31Z" />
