2c0c49a76b5b4d26744b2ff83f58fa6173e6d794
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2023-12-15 23:25:12 +0530

    Pan element is set according to the path

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,13 +14,21 @@ import {
   StackOverflowIcon,
   MediumIcon,
 } from "./Icons";
+import { useRouter } from "next/router";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
   return (
     <Link href={href} className={`${className} relative group`}>
       {title}
 
-      <span className="h-[1.5px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300">&nbsp;</span>
+      <span
+        className={`h-[1.5px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        } `}
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
