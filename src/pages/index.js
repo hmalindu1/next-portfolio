42cf8bfaa2c04468f875d79eb82f2dde46c363c6
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2024-01-01 23:58:32 +0530

    added css for main tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=''>
+      <main className='flex items-center text-dark w-full min-h-100vh'>
         HOME
       </main>
     </>
